719dbbebdf12cd56dfb4f52416e9b9fd84dc897a
dkdkdk jung <ehdrb307@gmail.com>
2021-04-08 07:44:52 +0000

    두번째

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 컨트롤러 같은역할 GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IoT 익스프레스Express' });
 });
 
 module.exports = router;
